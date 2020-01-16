0eabcfee4ee40faa295a1217151722d18b1205f6
sandy1618 <sandeepnayak1.618@gmail.com>
2020-01-17 08:27:41 +0900

    solved the Yahoo_test_2.py problem

--- CodingInterviews/yahoo_test_2.py
+++ CodingInterviews/yahoo_test_2.py
@@ -18,11 +18,13 @@ if __name__ == "__main__":
     _qty_list = []
     _total_price = []
     _total_qty = []
+    previous = 0
     for i in range(1, t+1):
         _list = input().split()
-        print(_list)
-        previous = i-1
-    # if date list is none then -d0-
+        # print(_list)
+
+
+    # if date list is none then -d0- # should run only once
         if not _date_list :
             _date_list.append(_list[date])
             _name_list.append(_list[name])
@@ -30,33 +32,39 @@ if __name__ == "__main__":
             _qty_list.append(_list[qty])
             _total_price.append(int(_list[price]) * int(_list[qty]))
             _total_qty.append(int(_list[qty]))
-            print("1")
+            # print("In loop 1")
+            # print(previous)
          #else  if new date is equal to previous date -do-
         else:
-            if _list[date] is _date_list[previous]:
+            # print(previous)
+            # print(_list[date])
+            # print(_date_list[previous])
+            if _list[date] == _date_list[previous]:
             # if new name is equal to previoes -do-
-                if _list[name] is _name_list[previous]:
+                if _list[name] == _name_list[previous]:
                     _total_price[previous] += (int(_list[price]) * int(_list[qty]))
                     _total_qty[previous] += int(_list[qty])
-                    print("2")
+                    # print("In loop 2")
             # else (new name not equal to previos -do
-                elif _list[name] is not _name_list[previous]: # note for simplicity, this does not take cares of same date but unordered name
+                else : # note for simplicity, this does not take cares of same date but unordered name
                     _date_list.append(_list[date])
                     _name_list.append(_list[name])
                     _price_list.append(_list[price])
                     _qty_list.append(_list[qty])
                     _total_price.append(int(_list[price]) * int(_list[qty]))
                     _total_qty.append(int(_list[qty]))
-                    print("3")
+                    previous+=1
+                    # print("In loop 3")
                     #else if new date not equal to previous date
 
-            elif _list[date] is not _date_list[previous]:
+            elif _list[date] != _date_list[previous]:
                 # get the index of maximum total price
                 index = _total_price.index(max(_total_price))
                 # print the informaiton out in the string.\
                 print("{} {} {}".format(_date_list[index], _name_list[index], _total_qty[index]))
-                print("4")
+                # print("In loop 4")
             #empty everything and start agisin
+                # flush list
                 _date_list = []
                 _name_list = []
                 _price_list = []
@@ -64,9 +72,18 @@ if __name__ == "__main__":
                 _total_price = []
                 _total_qty = []
 
+                # add presest data to _date_list
+                _date_list.append(_list[date])
+                _name_list.append(_list[name])
+                _price_list.append(_list[price])
+                _qty_list.append(_list[qty])
+                _total_price.append(int(_list[price]) * int(_list[qty]))
+                _total_qty.append(int(_list[qty]))
+
+                previous=0
                 continue
 
-
+    print("{} {} {}".format(_date_list[index], _name_list[index], _total_qty[index]))
 
 
 
