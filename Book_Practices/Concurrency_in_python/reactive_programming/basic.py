f703992538dc934b8a2d87dcea4910df653e4435
sandy1618 <sandeepnayak1.618@gmail.com>
2020-03-04 22:18:45 +0900

    Changes made in home pc

--- Book_Practices/Concurrency_in_python/reactive_programming/basic.py
+++ Book_Practices/Concurrency_in_python/reactive_programming/basic.py
@@ -1,9 +1,19 @@
 #! usr/bin/env python
 __author__ = 'Sandeep Kumar Nayak'
 __status__ = 'development'
+import sys
 
 from rx import Observable
 
+# argv = Observable.(sys.argv[1:])
+## adding capitalize functionality to the data of the observable
+argv = Observable.from_(sys.argv[1:]).map(lambda i:i.capitalize())
+argv.subscribe(
+    on_next = lambda i: print('on_next: {}'.format(i)),
+    on_error = lambda e:print("on_error: {}".format(e)),
+    on_completed = lambda : print("on_completed")
+
+               )
 
 # def push_five_strings(observer):
 #     observer.on_next('alpha')
